98aa83b119f92628d42941faf2d0614385df987f
Shubhamrana99 <shubhamrana19599@gmail.com>
2024-01-14 15:56:29 +0530

    feat : navbar with styling

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import { getImageurl } from "../../utils";
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        PortFolio
+      </a>
+
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageurl("nav/closeIcon.png")
+              : getImageurl("nav/menuIcon.png")
+          }
+          alt="menu-btn"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#skills">Skill</a>
+          </li>
+          <li>
+            <a href="#projects">Project</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
